610bd19fb52efca365cb5403d264215e0e745e7c
tonghoang <hoangtd241100@gmail.com>
2024-04-04 18:23:50 +0700

    update bg #7

--- src/pages/Info/Info.tsx
+++ src/pages/Info/Info.tsx
@@ -1,17 +1,17 @@
 import { Modal } from "@mui/material";
 import axios from "axios";
 import classNames from "classnames/bind";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import btn from "../../assets/images/btn.png";
 import logo from "../../assets/images/logo.png";
+import nextImg from "../../assets/images/next.png";
+import product from "../../assets/images/product.png";
 import text1 from "../../assets/images/text1.png";
 import text2 from "../../assets/images/text2.png";
 import text3 from "../../assets/images/text3.png";
 import text4 from "../../assets/images/text4.png";
-import nextImg from "../../assets/images/next.png";
-import product from "../../assets/images/product.png";
 import Loading from "../../component/loading/Loading";
 import styles from "./Info.module.scss";
 
