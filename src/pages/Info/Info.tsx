c2c2a98d5e0efa7e90a0dda0aa1b758633b0f3a1
tonghoang <hoangtd241100@gmail.com>
2024-04-04 16:50:08 +0700

    update bg #5

--- src/pages/Info/Info.tsx
+++ src/pages/Info/Info.tsx
@@ -75,99 +75,94 @@ const Info = () => {
               onSubmit={handleSubmit(handleSurvey)}
               className={cx("form-content")}
             >
-              {!imgLoaded && (
-                <>
-                  <div className={cx("form_input")}>
-                    <div className={cx("form-item")}>
-                      <label>Họ và tên: (*)</label>
-                      <input
-                        {...register("name", {
-                          required: true,
-                          //pattern: /^[A-Za-z]+$/i,
-                        })}
-                        placeholder="Full name"
-                      />
-                      <div className={cx("form-err")}>
-                        {errors.name?.type == "required" && (
-                          <p>This field is required</p>
-                        )}
-                        {errors.name?.type == "pattern" && (
-                          <p>Please type your name</p>
-                        )}
-                      </div>
-                    </div>
-                    <div className={cx("form-item")}>
-                      <label>Số điện thoại: (*)</label>
-                      <input
-                        {...register("phone", {
-                          required: true,
-                          minLength: 10,
-                          maxLength: 12,
-                          pattern: /((09|03|07|08|05)+([0-9]{8})\b)/g,
-                        })}
-                        placeholder="Phone number"
-                      />
-                      <div className={cx("form-err")}>
-                        {errors.phone?.type === "required" && (
-                          <p>This field is required</p>
-                        )}
-                        {errors?.phone?.type === "pattern" && (
-                          <p>Please type your phone number</p>
-                        )}
-                        {errors?.phone?.type === "minLength" && (
-                          <p>Please type your phone number</p>
-                        )}
-                        {errors?.phone?.type === "maxLength" && (
-                          <p>Please type your phone number</p>
-                        )}
-                      </div>
-                    </div>
-                    <div className={cx("form-item")}>
-                      <label>Email: (*)</label>
-                      <input
-                        {...register("mail", {
-                          required: true,
-                          pattern:
-                            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-                        })}
-                        placeholder="Email"
-                      />
-                      <div className={cx("form-err")}>
-                        {errors.mail?.type === "required" && (
-                          <p>This field is required</p>
-                        )}
-                        {errors.mail?.type === "pattern" && (
-                          <p>Please type your email</p>
-                        )}
-                      </div>
-                    </div>
+              <div className={cx("form_input")}>
+                <div className={cx("form-item")}>
+                  <label>Họ và tên: (*)</label>
+                  <input
+                    {...register("name", {
+                      required: true,
+                      //pattern: /^[A-Za-z]+$/i,
+                    })}
+                    placeholder="Full name"
+                  />
+                  <div className={cx("form-err")}>
+                    {errors.name?.type == "required" && (
+                      <p>This field is required</p>
+                    )}
+                    {errors.name?.type == "pattern" && (
+                      <p>Please type your name</p>
+                    )}
                   </div>
-                  <div className={cx("policy")}>
-                    <div className={cx("checkbox-wrapper-46")}>
-                      <input
-                        type="checkbox"
-                        id="cbx-46"
-                        className={cx("inp-cbx")}
-                        {...register("policy", {
-                          required: true,
-                        })}
-                      />
-                      <label htmlFor="cbx-46" className={cx("cbx")}>
-                        <span className={cx("svg")}>
-                          <svg viewBox="0 0 12 10" height="10px" width="12px">
-                            <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
-                          </svg>
-                        </span>
-                        <div className={cx("text3")}>
-                          <img src={text3} />
-                        </div>
-                      </label>
-                    </div>
+                </div>
+                <div className={cx("form-item")}>
+                  <label>Số điện thoại: (*)</label>
+                  <input
+                    {...register("phone", {
+                      required: true,
+                      minLength: 10,
+                      maxLength: 12,
+                      pattern: /((09|03|07|08|05)+([0-9]{8})\b)/g,
+                    })}
+                    placeholder="Phone number"
+                  />
+                  <div className={cx("form-err")}>
+                    {errors.phone?.type === "required" && (
+                      <p>This field is required</p>
+                    )}
+                    {errors?.phone?.type === "pattern" && (
+                      <p>Please type your phone number</p>
+                    )}
+                    {errors?.phone?.type === "minLength" && (
+                      <p>Please type your phone number</p>
+                    )}
+                    {errors?.phone?.type === "maxLength" && (
+                      <p>Please type your phone number</p>
+                    )}
                   </div>
-                </>
-              )}
+                </div>
+                <div className={cx("form-item")}>
+                  <label>Email: (*)</label>
+                  <input
+                    {...register("mail", {
+                      required: true,
+                      pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                    })}
+                    placeholder="Email"
+                  />
+                  <div className={cx("form-err")}>
+                    {errors.mail?.type === "required" && (
+                      <p>This field is required</p>
+                    )}
+                    {errors.mail?.type === "pattern" && (
+                      <p>Please type your email</p>
+                    )}
+                  </div>
+                </div>
+              </div>
+              <div className={cx("policy")}>
+                <div className={cx("checkbox-wrapper-46")}>
+                  <input
+                    type="checkbox"
+                    id="cbx-46"
+                    className={cx("inp-cbx")}
+                    {...register("policy", {
+                      required: true,
+                    })}
+                  />
+                  <label htmlFor="cbx-46" className={cx("cbx")}>
+                    <span className={cx("svg")}>
+                      <svg viewBox="0 0 12 10" height="10px" width="12px">
+                        <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+                      </svg>
+                    </span>
+                    <div className={cx("text3")}>
+                      <img src={text3} onLoad={() => setImgLoaded(false)} />
+                    </div>
+                  </label>
+                </div>
+              </div>
               <div className={cx("text4")}>
-                <img src={text4} onLoad={() => setImgLoaded(false)} />
+                <img src={text4} />
               </div>
               <div
                 className={cx("submit_btn")}
